f41e48613de4b2e156c47da9837a69d9b72af13a
emrecds <seyrelmeyen2@outlook.com>
2022-08-02 03:26:11 +0300

    fixes
    candy movement

--- puffy puffs/Assets/Scripts/board.cs
+++ puffy puffs/Assets/Scripts/board.cs
@@ -5,103 +5,179 @@ using UnityEngine;
 public class board : MonoBehaviour
 {
 
-    public int width;
-    public int height;
-    public int borderSize;
+	public int width;
+	public int height;
+
+	public float borderSize;
+
+	public GameObject tilePrefab;
+	public GameObject[] gamePiecePrefabs;
+
+	public float swapTime = 0.5f;
+
+	tile[,] m_allTiles;
+	gamePiece[,] m_allGamePieces;
+
+	tile m_clickedTile;
+	tile m_targetTile;
 
 
-    public GameObject tilePrefab;
-    public GameObject[] gamePiecePrefabs;
+	void Start()
+	{
+		m_allTiles = new tile[width, height];
+		m_allGamePieces = new gamePiece[width, height];
 
-    tile[,] m_allTiles;
-    gamePiece[,] m_allGamePieces;
+		SetupTiles();
+		SetupCamera();
+		FillRandom();
+	}
+
+	void SetupTiles()
+	{
+		for (int i = 0; i < width; i++)
+		{
+			for (int j = 0; j < height; j++)
+			{
+				GameObject tile = Instantiate(tilePrefab, new Vector3(i, j, 0), Quaternion.identity) as GameObject;
+
+				tile.name = "Tile (" + i + "," + j + ")";
+
+				m_allTiles[i, j] = tile.GetComponent<tile>();
+
+				tile.transform.parent = transform;
+
+				m_allTiles[i, j].Init(i, j, this);
+
+			}
+		}
+	}
+
+	void SetupCamera()
+	{
+		Camera.main.transform.position = new Vector3((float)(width - 1) / 2f, (float)(height - 1) / 2f, -10f);
+
+		float aspectRatio = (float)Screen.width / (float)Screen.height;
+
+		float verticalSize = (float)height / 2f + (float)borderSize;
+
+		float horizontalSize = ((float)width / 2f + (float)borderSize) / aspectRatio;
+
+		Camera.main.orthographicSize = (verticalSize > horizontalSize) ? verticalSize : horizontalSize;
+
+	}
+
+	GameObject GetRandomGamePiece()
+	{
+		int randomIdx = Random.Range(0, gamePiecePrefabs.Length);
+
+		if (gamePiecePrefabs[randomIdx] == null)
+		{
+			Debug.LogWarning("BOARD:  " + randomIdx + "does not contain a valid GamePiece prefab!");
+		}
+
+		return gamePiecePrefabs[randomIdx];
+	}
+
+	public void PlaceGamePiece(gamePiece gamePiece, int x, int y)
+	{
+		if (gamePiece == null)
+		{
+			Debug.LogWarning("BOARD:  Invalid GamePiece!");
+			return;
+		}
+
+		gamePiece.transform.position = new Vector3(x, y, 0);
+		gamePiece.transform.rotation = Quaternion.identity;
+
+		if (IsWithinBounds(x, y))
+		{
+			m_allGamePieces[x, y] = gamePiece;
+		}
+
+		gamePiece.SetCoord(x, y);
+	}
+
+	bool IsWithinBounds(int x, int y)
+	{
+		return (x >= 0 && x < width && y >= 0 && y < height);
+	}
+
+	void FillRandom()
+	{
+		for (int i = 0; i < width; i++)
+		{
+			for (int j = 0; j < height; j++)
+			{
+				GameObject randomPiece = Instantiate(GetRandomGamePiece(), Vector3.zero, Quaternion.identity) as GameObject;
+
+				if (randomPiece != null)
+				{
+					randomPiece.GetComponent<gamePiece>().Init(this);
+					PlaceGamePiece(randomPiece.GetComponent<gamePiece>(), i, j);
+					randomPiece.transform.parent = transform;
+
+				}
 
 
+			}
+		}
 
-    void Start()
-    {
-        m_allGamePieces = new gamePiece[width, height];
+	}
 
-        m_allTiles = new tile[width, height];
-        SetupTiles();
-        SetupCamera();
-        fillRandom();
-    }
+	public void ClickTile(tile tile)
+	{
+		if (m_clickedTile == null)
+		{
+			m_clickedTile = tile;
+			//Debug.Log("clicked tile: " + tile.name);
+		}
+	}
 
-    private void Update()
-    {
-      
-    }
+	public void DragToTile(tile tile)
+	{
+		if (m_clickedTile != null && IsNextTo(tile, m_clickedTile))
+		{
+			m_targetTile = tile;
+		}
+	}
 
-    void SetupTiles()
-    {
-        for (int i = 0; i < width; i++)
-        {
-            for (int j = 0; j < height; j++)
-            {
-                GameObject tile = Instantiate(tilePrefab, new Vector3(i, j, 9), Quaternion.identity) as GameObject;
-                tile.name = "Tile (" + i + "," + j + ")";
+	public void ReleaseTile()
+	{
+		if (m_clickedTile != null && m_targetTile != null)
+		{
+			SwitchTiles(m_clickedTile, m_targetTile);
+		}
 
-                m_allTiles[i, j] = tile.GetComponent<tile>();
-                tile.transform.parent = transform;
-                m_allTiles[i, j].Init(i, j, this);
-            }
+		m_clickedTile = null;
+		m_targetTile = null;
 
-        }
-    }
+	}
 
-    void SetupCamera()
-    {
-        Camera.main.transform.position = new Vector3((float)(width-1) / 2f, (float)(height-1) / 2f, -10f);
+	void SwitchTiles(tile clickedTile, tile targetTile)
+	{
 
-        float aspectRatio = (float)Screen.width / (float)Screen.height;
+		gamePiece clickedPiece = m_allGamePieces[clickedTile.xIndex, clickedTile.yIndex];
+		gamePiece targetPiece = m_allGamePieces[targetTile.xIndex, targetTile.yIndex];
 
-        float verticalSize = height / 2f + (float)borderSize;
+		clickedPiece.Move(targetTile.xIndex, targetTile.yIndex, swapTime);
+		targetPiece.Move(clickedTile.xIndex, clickedTile.yIndex, swapTime);
 
-        float horizontalSize = ((float)width / 2f + (float)borderSize / aspectRatio);
 
-        Camera.main.orthographicSize = (verticalSize > horizontalSize) ? verticalSize : horizontalSize;
+	}
 
-    }
+	bool IsNextTo(tile start, tile end)
+	{
+		if (Mathf.Abs(start.xIndex - end.xIndex) == 1 && start.yIndex == end.yIndex)
+		{
+			return true;
+		}
 
-    GameObject GetRandomGamePiece()
-    {
-        int randomIdx = Random.Range(0, gamePiecePrefabs.Length);
+		if (Mathf.Abs(start.yIndex - end.yIndex) == 1 && start.xIndex == end.xIndex)
+		{
+			return true;
+		}
 
-        if (gamePiecePrefabs[randomIdx] == null)
-        {
-            Debug.LogWarning("BOARD:  " + randomIdx + "does not contain a valid GamePiece prefab!");
-        }
+		return false;
+	}
 
-        return gamePiecePrefabs[randomIdx];
-    }
-
-    void PlaceGamePiece(gamePiece gamePiece, int x, int y)
-    {
-        if (gamePiece == null)
-        {
-            Debug.LogWarning("board: Invalid GamePiece!");
-            return;
-
-        }
-        gamePiece.transform.position = new Vector3(x, y, 0);
-        gamePiece.transform.rotation = Quaternion.identity;
-        gamePiece.SetCoord(x, y);
-
-    }
-
-    void fillRandom()
-    {
-        for (int i = 0; i < width; i++)
-        {
-            for (int j = 0; j < height; j++)
-            {
-                GameObject randomPiece = Instantiate(GetRandomGamePiece(),Vector3.zero,Quaternion.identity)as GameObject;
-                if (randomPiece != null)
-                {
-                    PlaceGamePiece(randomPiece.GetComponent<gamePiece>(), i, j);
-                }
-            }
-        }
-    }
 }
--- puffy puffs/Assets/Scripts/tile.cs
+++ puffy puffs/Assets/Scripts/tile.cs
@@ -1,29 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class tile : MonoBehaviour
 {
-    public int xIndex;
-    public int yIndex;
+	public int xIndex;
+	public int yIndex;
 
-    board m_board;
+	board m_board;
 
+	// Use this for initialization
+	void Start()
+	{
 
-    void Start()
-    {
-        
-    }
+	}
 
-    void Update()
-    {
-        
-    }
+	public void Init(int x, int y, board board)
+	{
+		xIndex = x;
+		yIndex = y;
+		m_board = board;
 
-   public void Init(int x, int y, board board)
-    {
-        xIndex = x;
-        yIndex = y;
-        m_board = board;
-    }
+	}
+
+	void OnMouseDown()
+	{
+		if (m_board != null)
+		{
+			m_board.ClickTile(this);
+		}
+
+	}
+
+	void OnMouseEnter()
+	{
+		if (m_board != null)
+		{
+			m_board.DragToTile(this);
+		}
+
+	}
+
+	void OnMouseUp()
+	{
+		if (m_board != null)
+		{
+			m_board.ReleaseTile();
+		}
+
+	}
 }
